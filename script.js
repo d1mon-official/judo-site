852c7b6c06dff60c6b7241b1e647255fbdf40b5c
d1mon-official <dima8989977@gmail.com>
2026-03-09 14:01:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function changeLang(lang, btnElement) {
 
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init({ duration: 1000, once: true });
-    const savedLang = localStorage.getItem('preferredLang') || 'uk';
+    const savedLang = localStorage.getItem('preferredLang') || 'en';
     const btn = Array.from(document.querySelectorAll('.lang-node')).find(b => b.innerText.toLowerCase() === (savedLang === 'en' ? 'en' : savedLang.toLowerCase()));
     changeLang(savedLang, btn);
 });
